18f703d717f920e239c9c061fc8e64614e0a8184
lolito996 <alejoalconsejo00@hotmail.com>
2025-10-28 10:34:31 -0500

    fix: update docker from jenkis

--- jenkins/run-jenkins-windows.cmd
+++ jenkins/run-jenkins-windows.cmd
@@ -1,6 +1,14 @@
 @echo off
-REM Ejecutar Jenkins en Docker (Windows)
+REM Ejecutar Jenkins personalizado en Docker (Windows)
 set JENKINS_HOME=%~dp0\jenkins_home
 if not exist "%JENKINS_HOME%" mkdir "%JENKINS_HOME%"
-echo Iniciando Jenkins en Docker...
-docker run -d -p 8081:8080 -p 50000:50000 --name jenkins -v "%JENKINS_HOME%:/var/jenkins_home" -v "%~dp0\jenkins-config.xml:/var/jenkins_home/jenkins-config.xml" jenkins/jenkins:lts
+echo Iniciando Jenkins con Docker y Maven...
+
+docker run -d ^
+  -p 8081:8080 -p 50000:50000 ^
+  --name jenkins ^
+  -u root ^
+  -v "%JENKINS_HOME%:/var/jenkins_home" ^
+  -v /var/run/docker.sock:/var/run/docker.sock ^
+  -v "%~dp0\jenkins-config.xml:/var/jenkins_home/jenkins-config.xml" ^
+  jenkins-docker-maven
